b97b79786bf382913be6d690094964c10d04fe8b
snootfef <delingucanada@gmail.com>
2023-06-11 22:28:14 -0400

    here lol

---
+++ src/Menu.java
@@ -0,0 +1,87 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+
+public class Menu extends JPanel implements Runnable{
+    public static BufferedImage bg;
+    public static BufferedImage logo;
+    public static BufferedImage goose1;
+    public static BufferedImage goose2;
+    public static MenuButton playButton;
+    public static MenuButton levelButton;
+    public static MenuButton helpButton;
+    public static boolean goose = false;
+
+    public Menu() {
+        JPanel menu = new JPanel();
+        menu.setPreferredSize(new Dimension(1000, 800));
+
+        playButton = new MenuButton("play.png", "play1.png", 244, 500);
+        levelButton = new MenuButton("play.png", "play1.png", 460, 500);
+        helpButton = new MenuButton("help.png", "help1.png", 685, 500);
+        addMouseListener(playButton);
+        addMouseListener(levelButton);
+        addMouseListener(helpButton);
+
+        try {
+            logo =  ImageIO.read(new File("logo.png"));
+            bg = ImageIO.read(new File("bg.png"));
+            goose1 = ImageIO.read(new File("goose1.png"));
+            goose2 = ImageIO.read(new File("goose2.png"));
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        catch(IOException e) {
+            System.out.println(":(");
+        }
+
+        Thread thread = new Thread(this);
+		thread.start();
+        this.setFocusable(true);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(bg, 0, 0, null);
+        g.drawImage(logo, 120, 40, null);
+        playButton.draw(g);
+        levelButton.draw(g);
+        helpButton.draw(g);
+
+        if(!goose) {
+            g.drawImage(goose1, 40, 500, null);
+            goose = !goose;
+        }
+        else if(goose) {
+            g.drawImage(goose2, 40, 500, null);
+            goose = !goose;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        JFrame f = new JFrame("Menu Test");
+        Menu panel = new Menu();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setPreferredSize(new Dimension(1000, 800));
+        f.add(panel);
+        f.pack();
+        f.setVisible(true);
+    }
+
+    public void run() {
+        while (true) {
+            repaint();
+            try {
+                Thread.sleep(300);
+            } 
+            catch (Exception e) {
+            }
+        }
+    }
+}
---
+++ src/MenuButton.java
@@ -0,0 +1,72 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class MenuButton implements MouseListener, MouseMotionListener {
+    public BufferedImage img;
+    public BufferedImage img1;
+    public int imgX, imgY, mouseX, mouseY;
+    public boolean hovered = false;
+
+    public MenuButton(String fileName, String fileName1, int imgX, int imgY) {
+        try {
+            this.img = ImageIO.read(new File(fileName));
+            this.img1 = ImageIO.read(new File(fileName1));
+            this.imgX = imgX;
+            this.imgY = imgY;
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        catch(IOException e) {
+            System.out.println(":(");
+        }
+    }
+
+    public void draw(Graphics g) {
+        mouseX = (int)MouseInfo.getPointerInfo().getLocation().getX();
+        mouseY = (int)MouseInfo.getPointerInfo().getLocation().getY();
+
+        if(mouseX >= imgX && mouseX <= (imgX+130) && mouseY >= imgY && mouseY <= (imgY+134)) {
+            System.out.println("hovered");
+            g.drawImage(this.img1, this.imgX, this.imgY, null);
+        }
+        else
+            g.drawImage(this.img, this.imgX, this.imgY, null);
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        mouseX = e.getX();
+		mouseY = e.getY();
+
+        System.out.println("clicked");
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    public void mouseReleased(MouseEvent e) {
+      
+    }
+
+    public void mouseExited(MouseEvent e) {
+      
+    }
+
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        mouseX = e.getX();
+		mouseY = e.getY();
+
+    }
+}
